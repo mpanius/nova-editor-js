4e01f21aa13935a201342c8a873e13a18a18581e
Rehan Aslam <advoor@gmail.com>
2019-04-28 02:01:10 +0100

    Embed tool

--- resources/views/embed.blade.php
+++ resources/views/embed.blade.php
@@ -1,3 +1,14 @@
-<div class="editor-js-block editor-js-embed">
-    <iframe style="width:100%;" height="320" frameborder="0" allowfullscreen src="{{ $embed }}"></iframe>
+<div class="editor-js-block">
+    <table class="editor-js-embed">
+        <iframe
+                width="{{ $width }}px"
+                height="{{ $height }}px"
+                frameborder="0"
+                allowfullscreen=""
+                src="{{ $embed }}"></iframe>
+
+        <div class="caption">
+            {{ $caption }}
+        </div>
+    </table>
 </div>
